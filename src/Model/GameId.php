137c030d629dde6a3810da6dc2e37e29d1abd445
Chris Harrison <chris.harrison@funeralzone.co.uk>
2018-05-09 08:58:32 +0100

    Added trait for GameId

--- src/Model/GameId.php
+++ src/Model/GameId.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace ChrisHarrison\Citphp\Model;
 
+use Funeralzone\ValueObjectExtensions\ComplexScalars\UUIDTrait;
 use Funeralzone\ValueObjects\ValueObject;
 
 final class GameId implements ValueObject
 {
-
+    use UUIDTrait;
 }
